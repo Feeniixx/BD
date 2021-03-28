e643e45a6fce6a5eccf9fbb66cd7ec9d64d50d17
Feeniixx <70769391+Feeniixx@users.noreply.github.com>
2021-03-28 22:24:39 +0200

    cw3

---
+++ CW_3/CW_3/Controllers/StudentsController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CW_3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CW_3.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        List<Student> students = new List<Student>();
+
+        public StudentsController()
+        {
+            var studentStrings = System.IO.File.ReadAllLines(@"Data/students.csv");
+            foreach (var studentString in studentStrings)
+            {
+                var student = studentString.Split(',');
+                students.Add(new Student
+                {
+                    FirstName = student[0],
+                    LastName = student[1],
+                    IndexNumber = student[2],
+                    DateOFBirth = student[3],
+                    Studies = student[4],
+                    Form = student[5],
+                    Email = student[6],
+                    FatherName = student[7],
+                    MotherName = student[8]
+                }); ;
+            }
+        }
+        // api/students/{indexNumber}
+        [HttpGet("{indexNumber}")]
+        public IActionResult GetStudent(string indexNumber)
+        {
+            var res = students.Where(st => st.IndexNumber == indexNumber).FirstOrDefault();
+            return Ok(res.ToString());
+        }
+
+        [HttpGet]
+        public IActionResult GetStudents(string orderBy)
+        {
+            string res = "";
+            students.ForEach(st => res += st.ToString());
+            return Ok(res);
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            if (student.FirstName == null || student.LastName == null || student.DateOFBirth == null || student.Studies == null || student.Form == null || student.Email == null || student.FatherName == null || student.MotherName == null)
+            {
+                return Problem(" Nie wszystkie dane na temat studenta są kompletne");
+            }
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            students.Add(student);
+            return Ok(student);
+        }
+
+        [HttpPut("{indexNumber}")]
+        public IActionResult UpdateStudent(string indexNumber)
+        {
+            return Ok("Student was updated");
+        }
+
+        [HttpDelete("{indexNumber}")]
+         public IActionResult DeleteStudent(string indexNumber )
+        {
+            string res = " ";
+            
+            students.ForEach(st => res += st.ToString().Remove)
+            if (res != null)
+            {
+                return Ok(res);
+            }
+
+              return NotFound("You can not delete this student");
+
+        }
+
+
+    }
+}
---
+++ CW_3/CW_3/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿namespace CW_3.Models
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string IndexNumber { get; set; }
+        public string DateOFBirth { get; set; }
+        public string Studies { get; set; }
+        public string Form { get; set; }
+        public string Email { get; set; }
+        public string FatherName { get; set; }
+        public string MotherName { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} {IndexNumber} {DateOFBirth} {Studies} {Form} {Email} {FatherName} {MotherName}\n";
+        }
+    }
+}
